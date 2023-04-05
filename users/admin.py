c5dfd2a019f96fb44d10fe92aeab8a2b8796f039
Aizhan Maratkyzy <73554654+Aizhan-creator@users.noreply.github.com>
2023-04-05 23:54:38 +0600

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Cinema)
+admin.site.register(models.City)
+admin.site.register(models.Payment)
+admin.site.register(models.User)
+admin.site.register(models.CinemaHall)
+admin.site.register(models.Movie)
